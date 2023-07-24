5081ffbf54f947a520e6c88a8c1a8b68f917ea89
sureshvarshini <sureshv@tcd.ie>
2023-07-24 02:45:36 +0100

    Frontend: Beautified recommendation page: with carousal and sliders with arrows

--- frontend/src/components/RecipeRecommendation.js
+++ frontend/src/components/RecipeRecommendation.js
@@ -2,9 +2,11 @@ import React, { useEffect, useState } from 'react'
 import { Form, Button, Modal } from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
+import Slider from 'react-slick';
 import axios from 'axios'
-import { logout } from '../Auth'
 import FoodCard from './FoodCard'
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const RecipeRecommendationPage = () => {
     const [recommendedFoodProfiles, setRecommendedFoodProfiles] = useState('')
@@ -28,36 +30,57 @@ const RecipeRecommendationPage = () => {
             })
     }, [])
 
+    const settings = {
+        dots: true,
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        nextArrow: (
+            <div>
+                <div className="next-slick-arrow"> ⫸ </div>
+            </div>
+        ),
+        prevArrow: (
+            <div>
+                <div className="prev-slick-arrow"> ⫷ </div>
+            </div>
+        )
+    };
 
 
     return (
-
         <div className='container'>
-            <h1>Hi this is where you will see your list of recommeded recipes</h1>
+            <div class="d-flex justify-content-center">
+                <h3 style={{ fontWeight: 'bold', marginTop: '20px', textAlign: "center" }}>Greetings!! Your list of recommended recipes awaits you on this page.</h3>
+            </div>
+            <div class="d-flex">
+                <h3 style={{ fontWeight: 'bold', marginTop: '20px' }}>Curated selections just for you.</h3>
+            </div>
             <div className='recommended recipes'>
-                <h4>Recommended recipes</h4>
-                {
-                    Object.keys(recommendedFoodProfiles).map((mealType) => (
-                        <div key={mealType}>
-                            <h5>{mealType}</h5>
+                {Object.keys(recommendedFoodProfiles).map((mealType) => (
+                    <div key={mealType}>
+                        <h2 style={{ display: 'flex', justifyContent: 'center', color: 'white', marginTop: '30px', backgroundColor: '#FF0078', borderRadius: '10px', padding: 10 }}>{mealType}</h2>
+                        <Slider {...settings}>
                             {recommendedFoodProfiles[mealType].map((recipes) => (
                                 <div key={recipes.id}>
-                                <FoodCard
-                                    Name={recipes.Name}
-                                    Quantity={recipes.Quantity}
-                                    Calories={recipes.Calories}
-                                />
+                                    <FoodCard
+                                        Name={recipes.Name}
+                                        Quantity={recipes.Quantity}
+                                        Calories={recipes.Calories}
+                                    />
                                 </div>
                             ))}
-                        </div>
-                    ))}
+                        </Slider>
+                    </div>
+                ))}
             </div>
+
             <div className='similar food recipes '>
-                <h4>Foods similar to recommendations from above</h4>
-                {
-                    Object.keys(similarFoodProfiles).map((mealType) => (
-                        <div key={mealType}>
-                            <h5>{mealType}</h5>
+                <h3 style={{ fontWeight: 'bold', marginTop: '40px' }}>Similar food choices to the above suggestions.</h3>
+                {Object.keys(similarFoodProfiles).map((mealType) => (
+                    <div key={mealType}>
+                        <h2 style={{ display: 'flex', justifyContent: 'center', color: 'white', marginTop: '30px', backgroundColor: '#029922', borderRadius: '10px', padding: 10 }}>{mealType}</h2>
+                        <Slider {...settings}>
                             {similarFoodProfiles[mealType].map((recipes) => (
                                 <div key={recipes.id}>
                                     <FoodCard
@@ -66,13 +89,14 @@ const RecipeRecommendationPage = () => {
                                     />
                                 </div>
                             ))}
-                        </div>
-                    ))}
+                        </Slider>
+                    </div>
+                ))}
             </div>
             <div className='similar user food recipes '>
-                <h4>Foods liked by users similar to you</h4>
-                {
-                    similarUserFoodProfiles.map((recipes) => (
+                <h3 style={{ fontWeight: 'bold', marginTop: '40px' }}>Foods preferred by individuals similar to your taste.</h3>
+                <Slider {...settings}>
+                    {similarUserFoodProfiles.map((recipes) => (
                         <div key={recipes.id}>
                             <FoodCard
                                 Name={recipes.Name}
@@ -80,6 +104,7 @@ const RecipeRecommendationPage = () => {
                             />
                         </div>
                     ))}
+                </Slider>
             </div>
         </div >
     )
