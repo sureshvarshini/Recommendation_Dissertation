3fe0339b8e0c27e4a4e546d5861fdf6ebeaaf9b5
sureshvarshini <sureshv@tcd.ie>
2023-07-26 03:00:02 +0100

    Frontend: Water tracking page

--- frontend/src/components/WaterTracker.js
+++ frontend/src/components/WaterTracker.js
@@ -4,18 +4,23 @@ import { Link, useNavigate } from 'react-router-dom'
 import { useForm } from 'react-hook-form'
 import axios from 'axios'
 import { logout } from '../Auth'
+import '../css/WaterTracker.css'
+import water from "../assets/water.jpg"
 
 const WaterTrackerPage = () => {
     const [waterIntakeStatus, setWaterIntakeStatus] = useState('')
     const [waterIntakeDetails, setWaterIntakeDetails] = useState('')
+    const [waterAmount, setWaterAmount] = useState('')
     const { register, watch, handleSubmit, reset, formState: { errors } } = useForm()
     const navigate = useNavigate()
     let userId = localStorage.getItem('id')
 
     const logWater = (data) => {
-        console.log('yuhoo!')
         console.log(data)
-        const body = JSON.stringify(data)
+        const waterData = {
+            amount: data,
+        }
+        const body = JSON.stringify(waterData)
         const headers = {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
@@ -23,11 +28,9 @@ const WaterTrackerPage = () => {
 
         axios.post(`/water/${userId}`, body, { headers: headers })
             .then(response => {
-                console.log("Success from fetching water details.")
+                console.log("Success upadting water details.")
                 console.log(response.data)
-                
-                reset()
-                navigate('/recommendations/water')
+                window.location.reload(false)
             })
             .catch(error => {
                 console.log("Error occured from fetching water details.")
@@ -50,48 +53,48 @@ const WaterTrackerPage = () => {
     }, [])
 
     return (
-        <div className='container'>
-            {waterIntakeStatus == -1 &&
-                <div>You havent started logging yout water intake.
-                    <form>
-                        <Form.Group>
-                            <Form.Label>Log your water intake here (ml)</Form.Label>
-                            <Form.Control type="number"
-                                min='1'
-                                {...register("amount", { valueAsNumber: true })}
-                            />
-                        </Form.Group>
-                        <Form.Group>
-                            <Button as="sub" variant="primary" onClick={handleSubmit(logWater)}>Log water</Button>
-                        </Form.Group>
-                    </form>
-                </div>}
+        <div className='container justify-content-center align-items-center' style={{ padding: 10 }}>
+            <div className='card d-flex align-items-center'>
+                <img className='card-image' src={water} alt='water' />
+                <div className='card-header'>
+                    <div className='card-title-group'>
+                        {waterIntakeStatus == -1 &&
+                            <>
+                                <h5 className='card-title'>7 cups left</h5>
+                                <div className='card-text fst-italic'>Start Drinking! ({waterIntakeDetails.remaining_ml}ml remaining)</div>
+                                <small>Goal: 7 cups (1700ml)</small>
+                            </>
+                        }
+                        {waterIntakeStatus == 0 &&
+                            <>
+                                <h5 className='card-title'>{waterIntakeDetails.remaining_cups} cups left </h5>
+                                <div className='card-text fst-italic'>Keep Drinking! ({waterIntakeDetails.remaining_ml}ml remaining)</div>
+                                <small>Goal: 7 cups (1700ml)</small>
+                            </>
+                        }
+                        {waterIntakeStatus == 1 &&
+                            <>
+                                <h5 className='card-title'>DAILY GOAL reached!</h5>
+                                <div className='card-text fst-italic'>HURRAY, Keep it up!</div>
+                            </>
+                        }
+                    </div>
+                </div>
+            </div>
 
-            {waterIntakeStatus == 0 &&
-                <h2>Keep drinking! <br></br>You still have {waterIntakeDetails.remaining_cups} cups to drink for the day.
-                    <br></br>
-                    <form>
-                        <Form.Group>
-                            <Form.Label>Log your water intake here (ml)</Form.Label>
-                            <Form.Control type="number"
-                                min='1'
-                                {...register("amount", { valueAsNumber: true })}
-                            />
-                        </Form.Group>
-                        <Form.Group>
-                            <Button as="sub" variant="primary" onClick={handleSubmit(logWater)}>Log water</Button>
-                        </Form.Group>
-                    </form>
-                </h2>}
-
-            {waterIntakeStatus == 1 &&
-                <h2>Hurray! You have reached your daily water intake goal!
-                    <p>Take me <Link to='/'>home</Link></p>
-                </h2>
-
-
-            }
+            <form style={{ padding: 50 }}>
+                <h3>Log your water intake here (ml).</h3>
+                <Button as="sub" variant="primary" class="btn btn-primary btn-block btn-lg" style={{ margin: 10 }} onClick={() => logWater(100)}>100 ml</Button>
+                <Button as="sub" variant="primary" class="btn btn-primary btn-block btn-lg" style={{ margin: 10 }} onClick={() => logWater(250)}>250 ml (1 cup)</Button>
+                <Button as="sub" variant="primary" class="btn btn-primary btn-block btn-lg" style={{ margin: 10 }} onClick={() => logWater(500)}>500 ml (2 cups)</Button>
+                <Button as="sub" variant="primary" class="btn btn-primary btn-block btn-lg" style={{ margin: 10 }} onClick={() => logWater(750)}>750 ml (3 cups)</Button>
+                <Button as="sub" variant="primary" class="btn btn-primary btn-block btn-lg" style={{ margin: 10 }} onClick={() => logWater(1000)}>1000 ml (4 cups)</Button>
+                <Button as="sub" variant="primary" class="btn btn-primary btn-block btn-lg" style={{ margin: 10 }} onClick={() => logWater(1250)}>1250 ml (5 cups)</Button>
+                <Button as="sub" variant="primary" class="btn btn-primary btn-block btn-lg" style={{ margin: 10 }} onClick={() => logWater(1500)}>1500 ml (6 cups)</Button>
+                <Button as="sub" variant="primary" class="btn btn-primary btn-block btn-lg" style={{ margin: 10 }} onClick={() => logWater(1750)}>1750 ml (7 cups)</Button>
+            </form>
         </div>
+
     )
 }
 
